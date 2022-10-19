1851543bbb1a54015a6a4ba726876902e3c51bdb
Travis Williams <traviswilliams997@gmail.com>
2022-10-18 19:45:16 -0500

    Edit hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,9 @@ const Hero = () => {
             <div>
                 <img className='w-full' src={bgImg} alt="/" />
             </div>
-            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[-5%] 
-            mx-1 md:left-1/2 transform  md:translate-x-1/2 bg-zinc-200 
-            borderborder-slate-300 rounded-xl text-center shadow-xl'>
+            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
+            border border-slate-300 rounded-xl text-center shadow-xl'>
                 <p>Data Services</p>
                 <div className='flex justify-between flex-wrap px-4'>
                 <p className= 'flex px-4 py-2 text-slate-500' ><CloudArrowUpIcon className='h-6 text-indigo-600'/> App Security</p>
