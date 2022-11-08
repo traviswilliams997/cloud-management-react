b01834276dace1d785ad49aab0808c36fefbbfcf
Travis Williams <traviswilliams997@gmail.com>
2022-11-07 21:32:27 -0500

    Create AllInOne file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Support from './components/Support';
+import AllInOne from './components/AllInOne';
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
     <Hero />   
     <About />
     <Support />
+    <AllInOne />
     </>
   );
 }
