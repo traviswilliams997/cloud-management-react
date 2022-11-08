0c44280b76eb200dc035c1fb176b78acbe0db49a
Travis Williams <traviswilliams997@gmail.com>
2022-11-07 21:27:39 -0500

    Edit support

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {PhoneIcon, ArrowSmallRightIcon} from '@heroicons/react/24/outline';
-import {CpuChipIcon, SupportIcon} from '@heroicons/react/24/solid';
+import {CpuChipIcon, GlobeAltIcon} from '@heroicons/react/24/solid';
 import supportImg from '../assets/support.jpg' 
 
 const Support = () => {
@@ -21,15 +21,35 @@ const Support = () => {
               obcaecati consequuntur.</p>
         </div>
 
-        <div className='grid grid-cols lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text:black'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
           <div className='bg-white rounded-xl shadow-2xl'>
             <div className='p-8'>
               <PhoneIcon  className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
-              <h3>Sales</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quod?</p>
+              <h3  className='font-bold text-2xl my-6 '>Sales</h3>
+              <p className='text-gray-600 text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quod?</p>
             </div>
-            <div>
-              <p>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+            <div className='bg-slate-100 pl-8 py-4'>
+              <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+            </div>
+          </div>      
+          <div className='bg-white rounded-xl shadow-2xl'>
+            <div className='p-8'>
+              <GlobeAltIcon  className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+              <h3  className='font-bold text-2xl my-6 '>Technical Support </h3>
+              <p className='text-gray-600 text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quod?</p>
+            </div>
+            <div className='bg-slate-100 pl-8 py-4'>
+              <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+            </div>
+          </div>      
+          <div className='bg-white rounded-xl shadow-2xl'>
+            <div className='p-8'>
+              <CpuChipIcon  className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+              <h3  className='font-bold text-2xl my-6 '>Media Inquires</h3>
+              <p className='text-gray-600 text-xl '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, quod?</p>
+            </div>
+            <div className='bg-slate-100 pl-8 py-4'>
+              <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
             </div>
           </div>          
         </div>
